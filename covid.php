436f3ae72099b7a91ad2571083f1ec3ed5c98310
Drew Tada <43223192+tadad@users.noreply.github.com>
2020-08-20 16:25:07 -0700

    Added links to Github Projects

---
+++ covid.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>AI Predicts COVID-19 Spread</title>
+      <meta http-equiv = "refresh" content = "2; url = https://github.com/tadad/ai_covid" />
+   </head>
+   <body>
+   </body>
+</html>
---
+++ digits.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>AI Recognizes Handwritten Digits</title>
+      <meta http-equiv = "refresh" content = "2; url = https://github.com/tadad/ai_mnist" />
+   </head>
+   <body> 
+   </body>
+</html>
